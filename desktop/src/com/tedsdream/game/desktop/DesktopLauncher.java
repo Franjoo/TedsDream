6a982bbe0e505668f16f8f1a50c47d4fb80e1873
Franjoo <franz.benthin.dev@gmail.com>
2014-05-22 18:44:03 +0200

    vSyncDisabled

--- desktop/src/com/tedsdream/game/desktop/DesktopLauncher.java
+++ desktop/src/com/tedsdream/game/desktop/DesktopLauncher.java
@@ -10,6 +10,7 @@ public class DesktopLauncher {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 
         config.title = "Teds Dream";
+        config.vSyncEnabled = false;
 
 		new LwjglApplication(new Main(), config);
 	}
