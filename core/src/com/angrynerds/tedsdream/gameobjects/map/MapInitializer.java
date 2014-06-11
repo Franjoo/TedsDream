9c56fef10fde40ca940ec3f154298211e7dc58a6
Franjoo <franz.benthin.dev@gmail.com>
2014-06-11 11:36:07 +0200

    create MapInitialzisation

--- core/src/com/angrynerds/tedsdream/gameobjects/map/MapInitializer.java
+++ core/src/com/angrynerds/tedsdream/gameobjects/map/MapInitializer.java
@@ -1,6 +1,9 @@
 package com.angrynerds.tedsdream.gameobjects.map;
 
+import com.angrynerds.tedsdream.gameobjects.Enemy;
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.MapObjects;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.utils.Array;
@@ -10,23 +13,32 @@ import com.badlogic.gdx.utils.Array;
  */
 public class MapInitializer implements ApplicationListener{
 
-    Array<SpawnController.SpawnObject> spawn;
+    private Array<Enemy> enemies;
 
     public MapInitializer(String path){
 
         TiledMap map = new TmxMapLoader().load(path);
 
+        // enemies
+        for (int i = 0; i < map.getLayers().getCount(); i++) {
+            MapObjects objects = map.getLayers().get(i).getObjects();
+            for (int j = 0; j < objects.getCount(); j++) {
+                MapObject o = objects.get(j);
+
+                String type = o.getProperties().get("type").toString();
+
+                switch (type){
+                    case "spawn":
+                        break;
+                    default:
+                }
 
 
-
+            }
+        }
 
     }
 
-    class Spawn{
-
-
-
-    }
 
     @Override
     public void create() {
--- desktop/src/com/angrynerds/tedsdream/game/desktop/DesktopLauncher.java
+++ desktop/src/com/angrynerds/tedsdream/game/desktop/DesktopLauncher.java
@@ -17,8 +17,8 @@ public class DesktopLauncher {
 
         Main main = new Main();
 
-        new LwjglApplication(main, config);
-//        new LwjglApplication(new MapInitializer("maps/map_05.tmx"), config);
+//        new LwjglApplication(main, config);
+        new LwjglApplication(new MapInitializer("maps/map_05.tmx"), config);
 
     }
 }
