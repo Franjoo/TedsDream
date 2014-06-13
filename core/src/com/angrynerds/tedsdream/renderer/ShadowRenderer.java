441c929d4f3a40d7db931dd9bd892c4e759fc7f1
osym49 <sebastian-bernard@arcor.de>
2014-06-13 11:28:38 +0200

    better animations and shadows

--- core/src/com/angrynerds/tedsdream/renderer/ShadowRenderer.java
+++ core/src/com/angrynerds/tedsdream/renderer/ShadowRenderer.java
@@ -14,16 +14,14 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
  */
 public class ShadowRenderer {
 
-<<<<<<< HEAD:core/src/com/angrynerds/renderer/ShadowRenderer.java
+
     Texture tex;
 
-=======
     private ShapeRenderer renderer = new ShapeRenderer();
-    private Camera camera;
->>>>>>> fa2d67ded173ea0131e54ff25ef7125c44aa6624:core/src/com/angrynerds/tedsdream/renderer/ShadowRenderer.java
+
 
     public ShadowRenderer(Camera camera){
-        this.camera = camera;
+
     }
 
     public void update(float deltaTime){
@@ -36,33 +34,14 @@ public class ShadowRenderer {
 
     }
 
-<<<<<<< HEAD:core/src/com/angrynerds/renderer/ShadowRenderer.java
-    public void renderShadow(Batch batch,Camera camera,GameObject object){
-         tex = new Texture(Gdx.files.internal("test/shadow.png"));
-         batch.begin();
+
+    public void renderShadow(Batch batch,GameObject object) {
+        tex = new Texture(Gdx.files.internal("test/shadow.png"));
+        batch.begin();
 
 
-
-        batch.draw(tex,object.getX()-100,object.getY()-50,200,100);
+        batch.draw(tex, object.getX() - 100, object.getY() - 50, 200, 100);
         batch.end();
-//        Gdx.gl.glEnable(GL20.GL_BLEND);
-//        Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
-//        renderer.setProjectionMatrix(camera.combined);
-//        renderer.begin(ShapeRenderer.ShapeType.Filled);
-//        renderer.setColor(0,0,0,0.5f);
-//        renderer.ellipse(object.getX()-object.getWidth()/2,object.getY(),object.getWidth()*0.7f,object.getHeight()/3);
-//        renderer.end();
-
-=======
-    public void renderShadow(GameObject object){
-        camera.update();
-        Gdx.gl.glEnable(GL20.GL_BLEND);
-        Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
-        renderer.setProjectionMatrix(camera.combined);
-        renderer.begin(ShapeRenderer.ShapeType.Filled);
-        renderer.setColor(0,0,0,0.5f);
-        renderer.ellipse(object.getX()-object.getWidth()/2,object.getY(),object.getWidth()*0.7f,object.getHeight()/3);
-        renderer.end();
->>>>>>> fa2d67ded173ea0131e54ff25ef7125c44aa6624:core/src/com/angrynerds/tedsdream/renderer/ShadowRenderer.java
     }
+
 }
